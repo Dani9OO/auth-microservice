726280c9df3de6a23edf76af9f799e0f58d2e598
Daniel Sánchez <dani9oo.dev@gmail.com>
2021-10-30 17:53:09 -0500

    ➕ Added service creation and querying route

--- src/service/service.routes.ts
+++ src/service/service.routes.ts
@@ -1,7 +1,13 @@
+import { plainToClass } from 'class-transformer'
+import { validate } from 'class-validator'
 import { Request, Response, Router } from 'express'
-import { RoutingController } from '../common/classes/routing-controller.class'
+import { ModuleRouting } from '../common/classes/module-routing.class'
+import { handleServerError } from '../common/utils/handle-server-error.util'
+import { ServiceController } from './service.controller'
+import { CreateServiceInput } from './service.inputs'
+import { handleValidationError } from '../common/utils/handle-validation-error.util'
 
-export class ServiceRouting implements RoutingController {
+export class ServiceRouting implements ModuleRouting {
   public readonly path = '/service'
 
   public readonly router = Router()
@@ -11,10 +17,32 @@ export class ServiceRouting implements RoutingController {
   }
 
   private initRoutes () {
+    this.router.get(`${this.path}/`, this.queryServices)
     this.router.post(`${this.path}/`, this.createService)
   }
 
-  private createService = async (request: Request, response: Response) => {
+  private queryServices = async (request: Request, response: Response) => {
+    try {
+      const services = await ServiceController.queryServices()
+      if (!(services.length > 0)) {
+        const error = new Error('There are no registered services on the database')
+        console.error(error)
+        return response.status(404).json({ success: false, message: error.message })
+      }
+      const message = `Successfully queried ${services.length} services`
+      console.log(message)
+      return response.status(200).json({ success: true, result: services, message })
+    } catch (error) { return response.status(500).json(handleServerError(error)) }
+  }
 
+  private createService = async (request: Request, response: Response) => {
+    try {
+      const data = plainToClass(CreateServiceInput, request.body)
+      const errors = await validate(data, { validationError: { target: false }, forbidUnknownValues: true })
+      if (errors.length > 0) return response.status(400).json(handleValidationError(errors))
+      const service = await ServiceController.createService(data)
+      const message = `Sucessfully created service with _id "${service._id}"`
+      return response.status(200).json({ success: true, result: { ...service }, message })
+    } catch (error) { return response.status(500).json(handleServerError(error)) }
   }
 }
