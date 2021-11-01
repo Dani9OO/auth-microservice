4cb03d62ba0aa58530621e01896273c9ce7de1d6
Daniel Sánchez <dani9oo.dev@gmail.com>
2021-10-31 19:23:32 -0600

    🖥️ Initialized server logic

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,12 @@
+import { Database } from './database'
+import { App } from './app'
+import { ServiceRouting } from './service/service.routes';
 (async () => {
-
+  if (!process.env.DB_URI) throw new Error('Database URI is not specified')
+  const database = new Database()
+  await database.init(process.env.DB_URI)
+  const app = new App([
+    new ServiceRouting()
+  ])
+  app.listen(Number(process.env.PORT) || 3000, process.env.LISTEN || '127.0.0.0')
 })()
