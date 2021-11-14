4940dcda83252f47466f4c968545805cfcbd9645
Daniel Sánchez <dani9oo.dev@gmail.com>
2021-11-14 00:05:41 -0600

    🥣 Changed register route to use it from services

--- src/user/user.routes.ts
+++ src/user/user.routes.ts
@@ -6,6 +6,7 @@ import { validate } from 'class-validator'
 import { handleValidationError } from '../common/utils/handle-validation-error.util'
 import { handleServerError } from '../common/utils/handle-server-error.util'
 import { UserController } from './user.controller'
+import { auth } from '../common/middleware/auth-service.middleware'
 export class UserRouting implements ModuleRouting {
   public readonly path = '/user'
 
@@ -16,7 +17,7 @@ export class UserRouting implements ModuleRouting {
   }
 
   private initRoutes () {
-    this.router.post('/auth/register', this.registerUser)
+    this.router.post('/auth/register', auth, this.registerUser)
   }
 
   private registerUser = async (request: Request, response: Response) => {
@@ -24,7 +25,7 @@ export class UserRouting implements ModuleRouting {
       const data = plainToClass(CreateUserInput, request.body)
       const errors = await validate(data, { validationError: { target: false }, forbidUnknownValues: true })
       if (errors.length > 0) return response.status(400).json(handleValidationError(errors))
-      const user = await UserController.createUser(data)
+      const { user } = await UserController.createUser(data, request.service._id)
       const message = `Successfully registered user with _id "${user._id}"`
       return response.status(200).json({ success: true, result: user, message })
     } catch (error) { return response.status(500).json(handleServerError(error)) }
