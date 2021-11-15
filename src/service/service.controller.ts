f616c0438b46c03d65746da75724a6a340cc0573
Daniel Sánchez <dani9oo.dev@gmail.com>
2021-11-15 17:12:36 -0600

    📁 Now properly saving to dynamic path
    💾 Changed env file generated to only have the service passphrase
    
    🔍 Added method to find a service user
    
    ✍️ Started working on the jwt signing for authentication

--- src/service/service.controller.ts
+++ src/service/service.controller.ts
@@ -1,10 +1,11 @@
 import { ServiceModel } from './service.model'
 import randomatic from 'randomatic'
 import { join, resolve } from 'path'
-import { mkdir, writeFile } from 'fs/promises'
+import { mkdir, readFile, writeFile } from 'fs/promises'
 import generateKeypair from '../common/functions/generate-keypair.function'
 import { CreateServiceInput } from './service.inputs'
 import { hash } from 'argon2'
+import { sign } from 'jsonwebtoken'
 
 export class ServiceController {
   public static queryServices = async () => {
@@ -19,11 +20,11 @@ export class ServiceController {
     if (!s) throw new Error('Failed to create a new service due to an unexpected server error')
     const passphrase = randomatic('Aa00!!', 16)
     const { publicKey, privateKey } = await generateKeypair(passphrase)
-    const path = join(resolve(process.cwd()), 'keys', 'test')
+    const path = join(resolve(process.cwd()), 'keys', s._id.valueOf())
     await mkdir(path, { recursive: true })
     await writeFile(join(path, 'private.pem'), privateKey)
     await writeFile(join(path, 'public.pem'), publicKey)
-    await writeFile(join(path, '.env'), `KEY_PASSPHRASE=${passphrase}\nAUTH_API_KEY=${apiKey}`)
+    await writeFile(join(path, 'passpharse'), passphrase)
     return { _id: s._id, apiKey, passphrase, publicKey }
   }
 
@@ -44,5 +45,16 @@ export class ServiceController {
 
   public static removeUserFromService = async (user: string, service: string) => {
     return await ServiceModel.findByIdAndUpdate(service, { $pull: { users: user } })
+  }
+
+  public static findUserInService = async (user: string, service: string) => {
+    return await ServiceModel.findOne({ _id: service, users: user })
+  }
+
+  public static signToken = async (payload: string | object | Buffer, service: string) => {
+    const path = join(resolve(process.cwd()), 'keys', service)
+    const key = await readFile(join(path, 'private.pem'))
+    const passphrase = (await readFile(join(path, 'passphrase'))).toString()
+    return sign(payload, { key, passphrase }, { expiresIn: '1h', algorithm: 'RS256' })
   }
 }
