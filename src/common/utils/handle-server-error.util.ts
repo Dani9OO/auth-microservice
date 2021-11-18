0da2dc8e8904bd79b32f854ca34ecedac3e82db0
Daniel Sánchez <dani9oo.dev@gmail.com>
2021-11-17 21:35:41 -0600

    🧑🏼‍🎨 Handling of custom errors

--- src/common/utils/handle-server-error.util.ts
+++ src/common/utils/handle-server-error.util.ts
@@ -1,5 +1,7 @@
+import { ResponseError } from '../errors/response.error'
+
 export const handleServerError = (error: any) => {
-  console.error(error)
+  if (!(error instanceof ResponseError)) console.error(error)
   const message = error instanceof Error ? error.message : 'Unexpected Server Error'
   return { success: false, message }
 }
