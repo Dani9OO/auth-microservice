861024b1beac2703f7a08eab114e4d56a7b511c7
Daniel Sánchez <dani9oo.dev@gmail.com>
2021-11-17 21:33:05 -0600

    ❎ Log errors to stderr

--- src/common/utils/handle-server-error.util.ts
+++ src/common/utils/handle-server-error.util.ts
@@ -1,5 +1,5 @@
 export const handleServerError = (error: any) => {
-  console.log(error)
+  console.error(error)
   const message = error instanceof Error ? error.message : 'Unexpected Server Error'
   return { success: false, message }
 }
