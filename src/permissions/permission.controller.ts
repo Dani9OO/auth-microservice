391947aa10f7db7355474782fb58316160537255
Daniel Sánchez <dani9oo.dev@gmail.com>
2021-11-17 22:06:41 -0600

    ⏭️ Added validation skip to create method 👊🏻 Removing permission from module on delete

--- src/permissions/permission.controller.ts
+++ src/permissions/permission.controller.ts
@@ -2,11 +2,11 @@ import { CreatePermissionInput, UpdatePermissionInput } from './permission.input
 import { PermissionModel } from './permission.model'
 import { ModuleController } from '../module/module.controller'
 export class PermissionController {
-  public static createPermission = async (permission: CreatePermissionInput, service: string) => {
-    await ModuleController.findModule(permission.module, service)
+  public static createPermission = async (permission: CreatePermissionInput, service: string, skipValidation?: boolean) => {
+    if (!skipValidation) await ModuleController.findModule(permission.module, service)
     const p = await PermissionModel.create(permission)
-    await ModuleController.addPermissionToModule(permission.module, p._id.valueOf())
-    return p
+    if (!skipValidation) await ModuleController.addPermissionToModule(permission.module, p._id.valueOf())
+    return p.toObject()
   }
 
   public static updatePermission = async (permission: UpdatePermissionInput, service: string) => {
@@ -15,7 +15,8 @@ export class PermissionController {
   }
 
   public static deletePermission = async (_id: string, service: string) => {
-    await ModuleController.findModule(_id, service)
+    const m = await ModuleController.findModule(_id, service)
+    await ModuleController.removePermissionFromModule(m._id, _id)
     return await PermissionModel.findByIdAndDelete(_id)
   }
 }
