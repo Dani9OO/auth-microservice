6c66faacf6b29ff4f3fdb2fb04fcf65fba9d5d0b
Daniel Sánchez <dani9oo.dev@gmail.com>
2021-11-17 22:09:39 -0600

    🔒 Added methods to handle module permissions 👷🏾 Refactored methods to properly manage changes

--- src/module/module.controller.ts
+++ src/module/module.controller.ts
@@ -1,17 +1,35 @@
 import { CreateModuleInput, UpdateModuleInput } from './module.input'
 import { ModuleModel } from './module.model'
+import { NotFoundError } from '../common/errors/not-found.error'
+import { PermissionController } from '../permissions/permission.controller'
 export class ModuleController {
+  public static findModule = async (m: string, service: string) => {
+    const moduleDoc = await ModuleModel.findOne({ _id: m, service }).lean()
+    if (!moduleDoc) throw new NotFoundError('Module', { name: '_id', value: m })
+    return moduleDoc
+  }
+
   public static createModule = async (service: string, m: CreateModuleInput) => {
-    return await ModuleModel.create({ service, ...m })
+    const moduleDoc = await ModuleModel.create({ service, name: m.name })
+    const permissions = await Promise.all(m.permissions.map(p => PermissionController.createPermission({ module: moduleDoc._id.valueOf(), name: p }, service, true)))
+    moduleDoc.permissions = permissions.map(p => p._id.valueOf())
+    await moduleDoc.save()
+    return moduleDoc
   }
 
   public static updateModule = async (service: string, m: UpdateModuleInput) => {
-    const _id = m._id
-    delete m._id
-    return await ModuleModel.findOneAndUpdate({ _id, service }, m)
+    return await ModuleModel.findOneAndUpdate({ _id: m._id, service }, { name: m.name })
   }
 
   public static deleteModule = async (service: string, _id: string) => {
     return await ModuleModel.findOneAndDelete({ _id, service })
+  }
+
+  public static addPermissionToModule = async (m: string, permission: string) => {
+    await ModuleModel.findByIdAndUpdate(m, { $push: { permissions: permission } })
+  }
+
+  public static removePermissionFromModule = async (m: string, permission: string) => {
+    await ModuleModel.findByIdAndUpdate(m, { $pull: { permissions: permission } })
   }
 }
