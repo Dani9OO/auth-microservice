6cfe56fbbf63d42b7c703b01d38f00d432b9eb69
Daniel Sánchez <dani9oo.dev@gmail.com>
2021-11-19 21:02:28 -0600

    🛡 Finished auth method

--- src/user/user.routes.ts
+++ src/user/user.routes.ts
@@ -5,11 +5,8 @@ import { plainToClass } from 'class-transformer'
 import { validate } from 'class-validator'
 import { handleValidationError } from '../common/utils/handle-validation-error.util'
 import { handleServerError } from '../common/utils/handle-server-error.util'
-import { UserController } from './user.controller'
+import { UserController } from './user.controller';
 import { auth } from '../common/middleware/auth-service.middleware'
-import { ServiceController } from '../service/service.controller'
-import { NotFoundError } from '../common/errors/not-found.error'
-import { UnauthorizedError } from '../common/errors/unauthorized.error'
 export class UserRouting extends Routing {
   public readonly resource = 'User'
 
@@ -41,11 +38,13 @@ export class UserRouting extends Routing {
       const data = plainToClass(AuthenticateUserInput, request.body)
       const errors = await validate(data, { validationError: { target: false }, forbidUnknownValues: true })
       if (errors.length > 0) return response.status(400).json(handleValidationError(errors))
-      const user = await UserController.findUserByEmail(data.email)
-      if (!user) return response.status(404).json(new NotFoundError(this.resource, { name: 'email', value: data.email }).respond())
-      const service = await ServiceController.findUserInService(user._id.valueOf(), request.service._id)
-      if (!service) return response.status(403).json(new UnauthorizedError(user._id.valueOf(), request.service._id).respond())
-      // TODO: Sign JWT with permissions payload
+      const { user, permissions } = await UserController.validateLogin(data.email, request.service._id)
+      const token = await UserController.signToken({ ...user, permissions }, request.service._id)
+      const { refreshToken, expires } = await UserController.generateRefreshToken(user.id, request.ip)
+      response.cookie('refreshToken', refreshToken, { httpOnly: true, secure: true, expires, sameSite: 'none' })
+      const message = `Successfully authenticated user with _id "${user.id}" to service ${request.service._id}`
+      console.log(message)
+      return response.header('Authorization', `Bearer ${token}`).status(200).json({ success: true, result: user, message })
     } catch (error) { return response.status(500).json(handleServerError(error)) }
   }
 }
