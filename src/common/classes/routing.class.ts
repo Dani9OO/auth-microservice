78284fbb06f7ab8f7fdb961c0063e53825da4f8d
Daniel Sánchez <dani9oo.dev@gmail.com>
2021-11-19 23:55:21 -0600

    ⚾ Made path a getter so it doesn't error out

--- src/common/classes/routing.class.ts
+++ src/common/classes/routing.class.ts
@@ -3,7 +3,9 @@ import { Router } from 'express'
 export class Routing {
   public readonly resource!: string
 
-  public readonly path: string = `/${this.resource.toLowerCase()}`
-
   public readonly router!: Router
+
+  public get path () {
+    return `/${this.resource.toLowerCase()}`
+  }
 }
