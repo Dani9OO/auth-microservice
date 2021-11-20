e092cccaf3691b9b22057773f607ecfff4de52f0
Daniel Sánchez <dani9oo.dev@gmail.com>
2021-11-19 23:11:01 -0600

    🔮 Cleaned up server code

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,40 @@
+import validateEnvironment from './common/functions/validate-environment'
+
 import { Database } from './database'
 import { App } from './app'
-import { ServiceRouting } from './service/service.routes';
+import { Mailer } from './common/mailer'
+
+import { ModuleRouting } from './module/module.routes'
+import { PermissionRouting } from './permissions/permission.routes'
+import { PolicyRouting } from './policy/policy.routes'
+import { RoleRouting } from './role/role.routes'
+import { ServiceRouting } from './service/service.routes'
+import { UserRouting } from './user/user.routes'
+
 (async () => {
-  if (!process.env.DB_URI) throw new Error('Database URI is not specified')
+  const env = validateEnvironment()
+
   const database = new Database()
-  await database.init(process.env.DB_URI)
+  await database.init(env.db.uri)
+
+  const mailer = new Mailer(env.smtp.host, env.smtp.port, { user: env.smtp.user, password: env.smtp.pass })
+  await mailer.verify()
+
+  const mod = new ModuleRouting()
+  const permission = new PermissionRouting()
+  const policy = new PolicyRouting()
+  const role = new RoleRouting()
+  const service = new ServiceRouting()
+  const user = new UserRouting()
+
   const app = new App([
-    new ServiceRouting()
+    mod,
+    permission,
+    policy,
+    role,
+    service,
+    user
   ])
+
   app.listen(Number(process.env.PORT) || 3000, process.env.LISTEN || '127.0.0.0')
 })()
