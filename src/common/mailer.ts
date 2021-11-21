df8f11721b44df46b353e8df92a2e0883aa5a044
Daniel Sánchez <dani9oo.dev@gmail.com>
2021-11-20 20:40:59 -0600

    📫 Added sender property to mailer class

--- src/common/mailer.ts
+++ src/common/mailer.ts
@@ -3,7 +3,10 @@ import { createTransport, Transporter } from 'nodemailer'
 export class Mailer {
   private _transporter: Transporter
 
+  public readonly sender: string
+
   constructor (host: string, port: number, auth: { user: string, pass: string }) {
+    this.sender = auth.user
     this._transporter = createTransport({ port, host, secure: true, auth })
   }
 
