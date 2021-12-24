892779a2f59f1f3a6a377203b0d156f877d00922
Daniel Sánchez <dev@daniromo.me>
2021-12-24 01:00:55 -0600

    🐧 Added route to auth admin

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
 import cors from 'cors'
 import { json } from 'body-parser'
-import express, { Application, urlencoded } from 'express'
+import express, { Application, Request, Response, urlencoded } from 'express'
 import { Routing } from './common/classes/routing.class'
 import cookieParser from 'cookie-parser'
+import { authAdmin } from './common/middleware/auth-admin.middleware'
 export class App {
   private app: Application
 
@@ -27,6 +28,7 @@ export class App {
   }
 
   private initRouters () {
+    this.app.post('/api/admin', authAdmin, (request: Request, response: Response) => response.status(200).json({ success: true, message: 'Successfully logged in' }))
     this.routers.forEach((r) => { this.app.use('/api', r.router) })
   }
 }
